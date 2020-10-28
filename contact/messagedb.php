0c1be1547fce4a06112f816b397effd8003e123b
Jotham Edoreh <jotham.edoreh@uniben.edu>
2020-10-28 13:37:24 +0100

    update contact 28102020 messages

--- contact/messagedb.php
+++ contact/messagedb.php
@@ -171,7 +171,7 @@ if (isset($_GET['page_no']) && $_GET['page_no']!="") {
 		$page_no = 1;
         }
 
-	$total_records_per_page = 3;
+	$total_records_per_page = 30;
     $offset = ($page_no-1) * $total_records_per_page;
 	$previous_page = $page_no - 1;
 	$next_page = $page_no + 1;
